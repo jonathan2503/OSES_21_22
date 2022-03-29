40d4654b6bfde986db7f75e8b1c3a9868a913935
omega <57135515+jonathan2503@users.noreply.github.com>
2022-03-29 19:30:47 +0200

    Delete Navigation directory

--- Navigation/navigate.h
+++
@@ -1,5 +0,0 @@
-#ifndef NAVIGATE
-#define NAVIGATE
-    int navigate(int [4], int *); // arguments: obstacle array, pointer to chosen direction
-    int initNavigation(int, int, int, int); // arguments: starting_row, starting_column, destination_row, destination_column
-#endif
